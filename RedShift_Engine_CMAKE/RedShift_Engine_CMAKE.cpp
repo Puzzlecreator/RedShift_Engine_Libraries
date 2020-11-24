70de579f82cccc17fd719d16278dbd22bf01105e
Puzzlecreator <48571170+Puzzlecreator@users.noreply.github.com>
2020-11-24 18:45:05 -0500

    Add project files.

---
+++ RedShift_Engine_CMAKE/RedShift_Engine_CMAKE.cpp
@@ -0,0 +1,12 @@
+﻿// RedShift_Engine_CMAKE.cpp : Defines the entry point for the application.
+//
+
+#include "RedShift_Engine_CMAKE.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ RedShift_Engine_CMAKE/RedShift_Engine_CMAKE.h
@@ -0,0 +1,8 @@
+﻿// RedShift_Engine_CMAKE.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
